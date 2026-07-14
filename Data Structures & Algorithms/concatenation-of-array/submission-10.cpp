7ddd1f0008be3c6d6f42d28e0ddfb7a78351911c
Gonçalo <gs.goncalosoares@gmail.com>
2026-07-14 22:03:06 +0200

    Add: concatenation-of-array - submission-10

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-10.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n * 2);
+        for (int i = 0; i < nums.size(); i++) {
+            ans.at(i) = nums.at(i); 
+            ans.at(n + i)  = nums.at(i);
+        }
+        return ans;
+    }
+};
